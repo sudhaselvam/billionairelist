a350f1e364f0e5cfe90e05a87d389c35120e0c9f
sudhaselvam <sudhahvbs@gmail.com>
2020-04-11 13:32:49 -0500

    refactor controller

--- src/main/java/com/targettech/springboot/crud/billionaireslist/controllers/BillionairesController.java
+++ src/main/java/com/targettech/springboot/crud/billionaireslist/controllers/BillionairesController.java
@@ -1,53 +1,50 @@
 package com.targettech.springboot.crud.billionaireslist.controllers;
 
 import com.targettech.springboot.crud.billionaireslist.entities.Billionaire;
-import com.targettech.springboot.crud.billionaireslist.repositories.BillionaireRepository;
+import com.targettech.springboot.crud.billionaireslist.service.BillionaireServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.thymeleaf.expression.Lists;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 
 @Controller
 public class BillionairesController {
-    private final BillionaireRepository billionaireRepository;
-    @Autowired
-    public BillionairesController(BillionaireRepository billionaireRepository){
-        this.billionaireRepository = billionaireRepository; }
 
-        @GetMapping("/")
-     public String showBillionaires(Model model) {
-         model.addAttribute("billionaires", billionaireRepository.findAll(Sort.by(Sort.Direction.DESC,"networth")));
-         return "index";
-     }
+    @Autowired
+     private  BillionaireServices billionaireServices;
+
+    // it will show index page with initial billionaire  list
+    @GetMapping("/")
+    public String showBillionaires(Model model) {
+        return loadBillionaires(model);
+    }
+    //show the  add billionaire form to enter data.
     @GetMapping("/addBillionaire")
     public String showAddBillionaireForm(Billionaire billionaire) {
         return "add-billionaire";
     }
+    // add billionaire and check the value either its valid or not if value is erorr
     @PostMapping("/addBillionaire")
     public String addBillionaire(@Valid Billionaire billionaire, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-billionaire";
         }
 
-        billionaireRepository.save(billionaire);
-        model.addAttribute("billionaires", billionaireRepository.findAll(Sort.by(Sort.Direction.DESC,"networth")));
-        return "index";
+        billionaireServices.save(billionaire);
+        return loadBillionaires(model);
     }
 
     @GetMapping("/updateBillionaire/{id}")
     public String showUpdateBillionaireForm(@PathVariable("id") long id, Model model) {
-        Billionaire billionaire = billionaireRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid billionaire Id:" + id));
-        model.addAttribute("billionaire", billionaire);
+       // model stores a billionaire object from service based on Id .
+        model.addAttribute("billionaire",billionaireServices.findById(id));
         return "update-billionaire";
     }
 
@@ -56,17 +53,22 @@ public class BillionairesController {
         if (result.hasErrors()) {
             billionaire.setId(id);
             return "update-billionaire";
+
         }
-        billionaireRepository.save(billionaire);
-        model.addAttribute("billionaires", billionaireRepository.findAll(Sort.by(Sort.Direction.DESC,"networth")));
+        billionaireServices.save(billionaire);
+        return loadBillionaires(model);
+    }
+
+    @GetMapping("/deleteBillionaire/{id}")
+    public String deleteContact(@PathVariable("id") long id, Model model) {
+        billionaireServices.delete(billionaireServices.findById(id));
+        return loadBillionaires(model);
+    }
+
+    private String loadBillionaires(Model model) {
+        //model stores sorted billionaire objects from service and return to index.
+        model.addAttribute("billionaires", billionaireServices.findSortedBillionaireNetWorth());
         return "index";
     }
-        @GetMapping("/deleteBillionaire/{id}")
-        public String deleteContact(@PathVariable("id") long id, Model model) {
-            Billionaire billionaire = billionaireRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid contact Id:" + id));
-            billionaireRepository.delete(billionaire);
-            model.addAttribute("billionaires", billionaireRepository.findAll(Sort.by(Sort.Direction.DESC,"networth")));
-            return "index";
-        }
-    }
+}
 
